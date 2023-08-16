02b77c17c99b353f1a35ba8591ab58bcc1e391ca
Juan Horta <jhorta89@hotmail.com>
2023-08-15 22:08:40 -0600

    Add functionality to buttons that modify local and visitor score

--- app/src/main/java/com/jhorta/basketballscore/MainActivity.kt
+++ app/src/main/java/com/jhorta/basketballscore/MainActivity.kt
@@ -2,10 +2,55 @@ package com.jhorta.basketballscore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jhorta.basketballscore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var localScore = 0
+    private var visitorScore = 0
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupButtons()
+
+    }
+
+    private fun setupButtons(){
+        binding.btnLocalMinus1.setOnClickListener {
+            localScore = calculateScore(localScore, -1)
+            binding.tvLocalScore.text = localScore.toString()
+        }
+
+        binding.btnLocalPlus1.setOnClickListener {
+            localScore = calculateScore(localScore, 1)
+            binding.tvLocalScore.text = localScore.toString()
+        }
+
+        binding.btnLocalPlus2.setOnClickListener {
+            localScore = calculateScore(localScore, 2)
+            binding.tvLocalScore.text = localScore.toString()
+        }
+
+        binding.btnVisitorMinus1.setOnClickListener {
+            visitorScore = calculateScore(visitorScore, -1)
+            binding.tvVisitorScore.text = visitorScore.toString()
+        }
+
+        binding.btnVisitorPlus1.setOnClickListener {
+            visitorScore = calculateScore(visitorScore, 1)
+            binding.tvVisitorScore.text = visitorScore.toString()
+        }
+
+        binding.btnVisitorPlus2.setOnClickListener {
+            visitorScore = calculateScore(visitorScore, 2)
+            binding.tvVisitorScore.text = visitorScore.toString()
+        }
+
+    }
+    private fun calculateScore(currentScore: Int, pointsDifference: Int): Int{
+        val score = currentScore + pointsDifference
+        return if (score < 0) 0 else score
     }
 }
